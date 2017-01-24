04b9470b8b637f280e30f43edc367f68abafcadf
Nicholas Hannah <nicjhannah@gmail.com>
2017-01-24 20:48:59 +1100

    Don't include atmosphere land-sea mask by default.

--- fv300_grid.py
+++ fv300_grid.py
@@ -8,19 +8,22 @@ from regular_grid import RegularGrid
 
 class FV300Grid(RegularGrid):
 
-    def __init__(self, num_lons, num_lats, num_levels, mask_file, description=''):
+    def __init__(self, num_lons, num_lats, num_levels, mask_file=None, description=''):
 
         levels = range(num_levels)
 
         self.type = 'Arakawa A'
 
-        with nc.Dataset(mask_file) as f:
-            try:
-                mask = np.round(f.variables['WGOCN'][0, 0, :, :-1])
-            except KeyError, e:
-                print("Error: can't find ocean fraction var WGOCN in {}.".format(mask_file),
-                       file=sys.stderr)
-                raise e
+        if mask_file:
+            with nc.Dataset(mask_file) as f:
+                try:
+                    mask = np.round(f.variables['WGOCN'][0, 0, :, :-1])
+                except KeyError, e:
+                    print("Error: var WGOCN not in {}.".format(mask_file),
+                           file=sys.stderr)
+                    raise e
+        else:            
+            mask = np.ones((num_lats, num_lons))
 
         assert mask.shape[0] == num_lats
         assert mask.shape[1] == num_lons
