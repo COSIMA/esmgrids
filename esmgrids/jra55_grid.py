51a126c0ada7c00a99416b241bb1c11888e82836
Nicholas Hannah <nicjhannah@gmail.com>
2017-04-28 12:58:21 +1000

    Use bounds to determing jra55 grid cell locations.

--- esmgrids/jra55_grid.py
+++ esmgrids/jra55_grid.py
@@ -11,8 +11,12 @@ class Jra55Grid(BaseGrid):
         self.full_name = 'JRA55'
 
         with nc.Dataset(h_grid_def) as f:
-            x_t = f.variables['lon'][:]
-            y_t = f.variables['lat'][1:-1]
+            lon_bnds = f.variables['lon_bnds'][:] 
+            lat_bnds = f.variables['lat_bnds'][:]
+            dx_t = lon_bnds[:, 1] - lon_bnds[:, 0]
+            dy_t = lat_bnds[:, 1] - lat_bnds[:, 0]
+            x_t = lon_bnds[:, 0] + dx_t[:] / 2
+            y_t = lat_bnds[:, 0] + dy_t[:] / 2
 
         super(Jra55Grid, self).__init__(x_t=x_t, y_t=y_t, description=description)
 
